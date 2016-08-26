689ea05de6ba830b92300b153f38c47140903f81
ccwittv <ccwittv@yahoo.com.ar>
2016-08-26 09:52:17 -0300

    Se empezó a trabajar a partir del TP 8. Toda esta aplicacion hay que pasarla a angular

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -4,15 +4,25 @@ B.	Mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un ter
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
 */
 
-function Rectangulo () 
-{
+var app = angular.module('ferreteConstruccion', []);//primer parametro es el nombre de modulo. puede tener muchos modulos
 
-}
-function Circulo () 
-{
-	
-}
-function Materiales () 
-{
-	
-}
+app.controller("controladorConstruccion", function($scope){
+
+	//puedo usar javascript en un controlador de angular
+	$scope.datos={};
+	$scope.Rectangulo=function(){
+		$scope.resultado= (parseInt($scope.datos.largo) * 2) + (parseInt($scope.datos.ancho) * 2);
+		return $scope.resultado;
+	}
+
+	$scope.Circulo=function(){
+		
+	}
+
+	$scope.Materiales=function(){
+		
+
+	}
+
+
+});//cada vez que se recarga la pagina se recarga el controlador
--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -3,7 +3,7 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
-function Sumar () 
+/*function Sumar () 
 {
 	
 }
@@ -14,4 +14,31 @@ function Promedio ()
 function PrecioFinal () 
 {
 	
-}
+}*/
+
+var app = angular.module('ferreteFactuacion', []);//primer parametro es el nombre de modulo. puede tener muchos modulos
+
+app.controller("controladorFacturacion", function($scope){
+
+	//puedo usar javascript en un controlador de angular
+	$scope.test="Hola Angular";
+	$scope.precio={};
+	$scope.Sumar=function(){
+		$scope.resultado= parseInt($scope.precio.uno) + parseInt($scope.precio.dos) + parseInt($scope.precio.tres);
+		return $scope.resultado;
+	}
+
+	$scope.Promedio=function(){
+		$scope.resultado=(parseInt($scope.precio.uno) + parseInt($scope.precio.dos) + parseInt($scope.precio.tres))/3;
+	}
+
+	$scope.PrecioFinal=function(){
+		$scope.resultado = $scope.Sumar() + ($scope.Sumar()*0.21);
+
+	}
+
+
+});//cada vez que se recarga la pagina se recarga el controlador
+
+
+
